456d632fbe61b61ab1ef3e168c036aa5e3c5b9d9
allea <abrash_han@hotmail.com>
2017-01-03 21:48:39 -0800

    fix: _wax_instance_userdata for ARC
    ARC doesn't allow id embedded in struct, use void * instead

--- lib/wax_instance.h
+++ lib/wax_instance.h
@@ -18,7 +18,7 @@
 
 
 typedef struct _wax_instance_userdata {
-    id instance;
+  /*id*/ void * instance; // ARC doesn't allow id embedded in struct, use void * instead
     BOOL isClass;
     Class isSuper; // isSuper not only stores whether the class is a super, but it also contains the value of the next superClass.
 	BOOL actAsSuper; // It only acts like a super once, when it is called for the first time.
